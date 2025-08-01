4b86ac774b3ff19c13ef14abb5c49af516c3dd62
Stockmite <rojogreenazul2@gmail.com>
2025-08-01 19:15:41 -0300

    I wrote the code necessary to bind the socket so it can listen to incomming connections. Yes there are suppost to be checks in place before you do this, too bad!

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <winsock2.h>
 #include <Windows.h>
+#include <ws2def.h>
 
 int main() {
-    
+
+    WSADATA wsadata;
+    WSAStartup(MAKEWORD(2,2), &wsadata);
+
+    SOCKET ServerSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = 0;
+    addr.sin_port = htons(8080);
+    bind(ServerSocket, &addr, sizeof(addr));
+
+
 }
