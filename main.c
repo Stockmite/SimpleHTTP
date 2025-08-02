537623034f79c9c379b50c68f4fbe1f3686260f3
Stockmite <rojogreenazul2@gmail.com>
2025-08-02 09:04:53 -0300

    Now the server should be able to not only read the contents of the client's requests, but also send the requested file (I think, I hope, I pray)
    I'll just test it later

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <winsock2.h>
 #include <Windows.h>
 #include <ws2def.h>
+#include <stdio.h>
 
 #define Buffer_lenght 256
 #define queue_len 5
@@ -25,5 +26,15 @@ int main() {
 
     char req_buffer[Buffer_lenght] = {0};
     recv(ClientSocket, req_buffer, Buffer_lenght, 0);
+
+    if (memcmp(req_buffer, "GET / ", 6) == 0) {
+
+        FILE* file = fopen("index.html", "r");
+
+        char fBuffer[Buffer_lenght] = {0};
+        fread(fBuffer, 1, Buffer_lenght, file);
+        send(ClientSocket, fBuffer, Buffer_lenght, 0);
+
+    }
 
 }
